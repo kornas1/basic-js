5d05e9ef6e4518dd28e97f98b22636468567012b
kornas1 <79769071+kornas1@users.noreply.github.com>
2021-03-28 16:17:37 +0300

    feat: add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater( str, options) {
+  let a = "";
+  let repeatTimes = options.repeatTimes;
+  let separator = options.separator;
+  let addition = options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let additionSeparator = options.additionSeparator;
+  if(typeof str != 'string') {str = String(str); }
+  if (repeatTimes === undefined){ repeatTimes = 1;}
+  if (separator === undefined){ separator = "+";}
+  if (addition === undefined){ addition = "";} 
+  if (additionRepeatTimes === undefined){additionRepeatTimes = 1;} 
+  if (additionSeparator === undefined){ additionSeparator = "|";}
+
+  for (let i = 0; i < additionRepeatTimes; i++){
+    if (i == additionRepeatTimes-1){
+      str += addition + separator;
+    }else{
+      str += addition + additionSeparator;
+    }
+  }
+  for (let i = 0; i < repeatTimes; i++){
+    if(i == repeatTimes - 1){
+      a += str.substr(0, str.length-separator.length);
+    }else{
+      a += str;
+    }
+  }
+  return a;
+  };
